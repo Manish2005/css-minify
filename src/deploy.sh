4f37a4665772eb95c658900e27e9e95c3621933b
Manish Gharat <gharatmanish@gmail.com>
2020-03-25 00:34:07 +0100

    debug

--- src/deploy.sh
+++ src/deploy.sh
@@ -4,8 +4,11 @@
 # APPNAME=${{secrets.APPNAME}}
 
 # Upload
+echo 'Uploading tar to server...'
+ls
+ls ./build-artifacts
 scp -o StrictHostKeyChecking=no -o ConnectTimeout=120 ./build-artifacts/$APPNAME.tar $USERNAME@$HOSTNAME:$BASEPATH/.tmp
-
+echo 'Making ssh connection to server...'
 ssh -o StrictHostKeyChecking=no -o ConnectTimeout=120 -l $USERNAME $HOSTNAME /bin/bash << EOF
     cd $BASEPATH/tools
     mkdir -p ./${APPNAME}1
