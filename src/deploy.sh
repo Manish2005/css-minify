c80e19ebbb0ebccd58a0541b0745ff80eed95946
Manish Gharat <gharatmanish@gmail.com>
2020-03-25 00:09:36 +0100

    added debug statements

--- src/deploy.sh
+++ src/deploy.sh
@@ -7,7 +7,10 @@
 scp -o StrictHostKeyChecking=no ./build-artifacts/$APPNAME.tar $USERNAME@$HOSTNAME:$BASEPATH/.tmp
 
 ssh -o StrictHostKeyChecking=no -l $USERNAME $HOSTNAME /bin/bash << EOF
+    pwd
+    ls
     cd $BASEPATH
+    pwd
     mkdir -p ./${APPNAME}1
     tar -C ./${APPNAME}1 -xvf ./.tmp/${APPNAME}.tar
 EOF
