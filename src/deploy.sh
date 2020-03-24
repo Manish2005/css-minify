69bdb30625d3b8eaea943c84027abf86b888e8d5
Manish Gharat <gharatmanish@gmail.com>
2020-03-25 00:26:31 +0100

    updated ssh connect timeout

--- src/deploy.sh
+++ src/deploy.sh
@@ -4,9 +4,9 @@
 # APPNAME=${{secrets.APPNAME}}
 
 # Upload
-scp -o StrictHostKeyChecking=no ./build-artifacts/$APPNAME.tar $USERNAME@$HOSTNAME:$BASEPATH/.tmp
+scp -o StrictHostKeyChecking=no -o ConnectTimeout=120 ./build-artifacts/$APPNAME.tar $USERNAME@$HOSTNAME:$BASEPATH/.tmp
 
-ssh -o StrictHostKeyChecking=no -l $USERNAME $HOSTNAME /bin/bash << EOF
+ssh -o StrictHostKeyChecking=no -o ConnectTimeout=120 -l $USERNAME $HOSTNAME /bin/bash << EOF
     cd $BASEPATH/tools
     mkdir -p ./${APPNAME}1
     tar -C ./${APPNAME}1 -xvf ./.tmp/${APPNAME}.tar
