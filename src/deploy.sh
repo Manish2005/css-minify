51f43790366f3b89c79ed3d7747dc243e23c06c0
Manish Gharat <gharatmanish@gmail.com>
2020-03-25 00:19:15 +0100

    fixed basepath

--- src/deploy.sh
+++ src/deploy.sh
@@ -7,11 +7,7 @@
 scp -o StrictHostKeyChecking=no ./build-artifacts/$APPNAME.tar $USERNAME@$HOSTNAME:$BASEPATH/.tmp
 
 ssh -o StrictHostKeyChecking=no -l $USERNAME $HOSTNAME /bin/bash << EOF
-    pwd
-    ls
-    cd $BASEPATH
-    pwd
-    ls
+    cd $BASEPATH/tools
     mkdir -p ./${APPNAME}1
     tar -C ./${APPNAME}1 -xvf ./.tmp/${APPNAME}.tar
 EOF
