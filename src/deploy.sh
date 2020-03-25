b76693bd110dd04d91d8bf3a1a30c733d0597584
Manish Gharat <gharatmanish@gmail.com>
2020-03-25 17:49:27 +0100

    updated ssh timeout

--- src/deploy.sh
+++ src/deploy.sh
@@ -7,9 +7,9 @@
 echo 'Uploading tar to server...'
 ls
 ls ./build-artifacts
-scp -o StrictHostKeyChecking=no -o ConnectTimeout=120 ./build-artifacts/$APPNAME.tar $USERNAME@$HOSTNAME:$BASEPATH/tools/.tmp
+scp -o StrictHostKeyChecking=no -o ConnectTimeout=240 ./build-artifacts/$APPNAME.tar $USERNAME@$HOSTNAME:$BASEPATH/tools/.tmp
 echo 'Making ssh connection to server...'
-ssh -o StrictHostKeyChecking=no -o ConnectTimeout=120 -l $USERNAME $HOSTNAME /bin/bash << EOF
+ssh -o StrictHostKeyChecking=no -o ConnectTimeout=240 -l $USERNAME $HOSTNAME /bin/bash << EOF
     cd $BASEPATH/tools
     mkdir -p ./${APPNAME}1
     tar -C ./${APPNAME}1 -xvf ./.tmp/${APPNAME}.tar
