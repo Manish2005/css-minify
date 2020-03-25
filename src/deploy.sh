8ab401289f36d7a9ee0428e898db1f9928ba51f5
Manish Gharat <gharatmanish@gmail.com>
2020-03-25 17:55:38 +0100

    cleanup artifacts after deployment

--- src/deploy.sh
+++ src/deploy.sh
@@ -5,12 +5,12 @@
 
 # Upload
 echo 'Uploading tar to server...'
-ls
-ls ./build-artifacts
 scp -o StrictHostKeyChecking=no -o ConnectTimeout=240 ./build-artifacts/$APPNAME.tar $USERNAME@$HOSTNAME:$BASEPATH/tools/.tmp
 echo 'Making ssh connection to server...'
 ssh -o StrictHostKeyChecking=no -o ConnectTimeout=240 -l $USERNAME $HOSTNAME /bin/bash << EOF
     cd $BASEPATH/tools
-    mkdir -p ./${APPNAME}1
-    tar -C ./${APPNAME}1 -xvf ./.tmp/${APPNAME}.tar
+    mkdir -p ./${APPNAME}
+    tar -C ./${APPNAME} -xvf ./.tmp/${APPNAME}.tar
+    rm ./.tmp/${APPNAME}.tar
 EOF
+echo 'Done'
