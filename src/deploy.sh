b593e35f9a082205f75b5f68cfcb26a38e0aff3e
Manish Gharat <gharatmanish@gmail.com>
2020-03-25 18:02:57 +0100

    added debug statements

--- src/deploy.sh
+++ src/deploy.sh
@@ -8,9 +8,12 @@ echo 'Uploading tar to server...'
 scp -o StrictHostKeyChecking=no -o ConnectTimeout=240 ./build-artifacts/$APPNAME.tar $USERNAME@$HOSTNAME:$BASEPATH/tools/.tmp
 echo 'Making ssh connection to server...'
 ssh -o StrictHostKeyChecking=no -o ConnectTimeout=240 -l $USERNAME $HOSTNAME /bin/bash << EOF
+    echo 'On remote server...'
     cd $BASEPATH/tools
     mkdir -p ./${APPNAME}
+    echo 'Extracting artifacts...'
     tar -C ./${APPNAME} -xvf ./.tmp/${APPNAME}.tar
+    echo 'cleaning up...'
     rm ./.tmp/${APPNAME}.tar
 EOF
 echo 'Done'
