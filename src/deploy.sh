8502466666f4972471a7ec8cc2efa7ee36c262ba
Manish Gharat <gharatmanish@gmail.com>
2020-03-25 19:12:58 +0100

    updated ssh connect retries

--- src/deploy.sh
+++ src/deploy.sh
@@ -5,9 +5,9 @@
 
 # Upload
 echo 'Uploading tar to server...'
-scp -o StrictHostKeyChecking=no -o ConnectTimeout=240 ./build-artifacts/$APPNAME.tar $USERNAME@$HOSTNAME:$BASEPATH/tools/.tmp
+scp -o StrictHostKeyChecking=no -o ConnectTimeout=240 -o ConnectionAttempts=3 ./build-artifacts/$APPNAME.tar $USERNAME@$HOSTNAME:$BASEPATH/tools/.tmp
 echo 'Making ssh connection to server...'
-ssh -o StrictHostKeyChecking=no -o ConnectTimeout=240 -l $USERNAME $HOSTNAME /bin/bash << EOF
+ssh -o StrictHostKeyChecking=no -o ConnectTimeout=240 -o ConnectionAttempts=3 -l $USERNAME $HOSTNAME /bin/bash << EOF
     echo 'On remote server...'
     cd $BASEPATH/tools
     mkdir -p ./${APPNAME}
